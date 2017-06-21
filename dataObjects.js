c091863efe391baec673c2954eff77d8a81c00b1
Anthony <prometheuszeroohone@gmail.com>
2017-06-21 17:21:34 +0100

    added upto Weapons company
    Need to add event listeners up to the same point

--- dataObjects.js
+++ dataObjects.js
@@ -616,10 +616,530 @@ const combatant = {
 			row: $('#weaponsPlatoon-IsEnemyRow')
 		},
 		stat: {
-			pow: 80,
-			def: 710,
+			pow: 224,
+			def: 1740,
 			components: ["platoonHQ", "mortarSection", "assaultSection", "mgSection"],
 			drops: "lightMachineGun"
+		}
+	},
+	rifleCompanyHQ:{
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#rifleCompanyHQ-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#rifleCompanyHQ-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#rifleCompanyHQ-enemyDisplay'),
+			row: $('#rifleCompanyHQ-IsEnemyRow')
+		},
+		stat: {
+			pow: 224,
+			def: 1740,
+			components: ["captain", "lieutenant", "staffSergeant", "staffSergeant", "corporal", "soldier"],
+			drops: "rifle"
+		}
+	},
+	rifleCompany: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#rifleCompany-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#rifleCompany-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#rifleCompany-enemyDisplay'),
+			row: $('#rifleCompany-IsEnemyRow')
+		},
+		stat: {
+			pow: 548,
+			def: 5320,
+			components: ["rifleCompanyHQ", "weaponsPlatoon", "riflePlatoon", "riflePlatoon", "riflePlatoon"],
+			drops: "rifle"
+		}
+	},
+	heavyMortarSquad: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#heavyMortarSquad-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarSquad-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarSquad-enemyDisplay'),
+			row: $('#heavyMortarSquad-IsEnemyRow')
+		},
+		stat: {
+			pow: 20,
+			def: 130,
+			components: ["corporal", "soldier", "soldier", "soldier", "soldier", "soldier"],
+			drops: "mortar80mm"
+		}
+	},
+	heavyMortarSectionHQ: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#heavyMortarSectionHQ-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarSectionHQ-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarSectionHQ-enemyDisplay'),
+			row: $('#heavyMortarSectionHQ-IsEnemyRow')
+		},
+		stat: {
+			pow: 8,
+			def: 110,
+			components: ["sergeant", "corporal", "soldier", "soldier"],
+			drops: "rifle"
+		}
+	},
+	heavyMortarSection: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#heavyMortarSection-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarSection-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarSection-enemyDisplay'),
+			row: $('#heavyMortarSection-IsEnemyRow')
+		},
+		stat: {
+			pow: 88,
+			def: 630,
+			components: ["heavyMortarSectionHQ", "heavyMortarSquad", "heavyMortarSquad", "heavyMortarSquad", "heavyMortarSquad"],
+			drops: "mortar80mm"
+		}
+	},
+	heavyMortarPlatoonHQ: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#heavyMortarPlatoonHQ-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarPlatoonHQ-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarPlatoonHQ-enemyDisplay'),
+			row: $('#heavyMortarPlatoonHQ-IsEnemyRow')
+		},
+		stat: {
+			pow: 10,
+			def: 190,
+			components: ["lieutenant", "staffSergeant", "sergeant", "soldier", "soldier"],
+			drops: "rifle"
+		}
+	},
+	heavyMortarPlatoon: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#heavyMortarPlatoon-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarPlatoon-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#heavyMortarPlatoon-enemyDisplay'),
+			row: $('#heavyMortarPlatoon-IsEnemyRow')
+		},
+		stat: {
+			pow: 186,
+			def: 1450,
+			components: ["heavyMortarPlatoonHQ", "heavyMortarSection", "heavyMortarSection"],
+			drops: "rifle"
+		}
+	},
+	antiArmourTeam: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#antiArmourTeam-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourTeam-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourTeam-enemyDisplay'),
+			row: $('#antiArmourTeam-IsEnemyRow')
+		},
+		stat: {
+			pow: 16,
+			def: 90,
+			components: ["corporal", "soldier", "soldier"],
+			drops: "rocketLauncher"
+		}
+	},
+	antiArmourSquad: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#antiArmourSquad-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourSquad-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourSquad-enemyDisplay'),
+			row: $('#antiArmourSquad-IsEnemyRow')
+		},
+		stat: {
+			pow: 34,
+			def: 220,
+			components: ["sergeant", "antiArmourTeam", "antiArmourTeam"],
+			drops: "rocketLauncher"
+		}
+	},
+	antiArmourSection: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#antiArmourSection-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourSection-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourSection-enemyDisplay'),
+			row: $('#antiArmourSection-IsEnemyRow')
+		},
+		stat: {
+			pow: 34,
+			def: 220,
+			components: ["staffSergeant", "antiArmourSquad", "antiArmourSquad"],
+			drops: "rocketLauncher"
+		}
+	},
+	antiArmourPlatoonHQ: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#antiArmourPlatoonHQ-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourPlatoonHQ-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourPlatoonHQ-enemyDisplay'),
+			row: $('#antiArmourPlatoonHQ-IsEnemyRow')
+		},
+		stat: {
+			pow: 6,
+			def: 120,
+			components: ["lieutenant", "sergeant", "soldier"],
+			drops: "rifle"
+		}
+	},
+	towSquad: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#towSquad-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#towSquad-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#towSquad-enemyDisplay'),
+			row: $('#towSquad-IsEnemyRow')
+		},
+		stat: {
+			pow: 30,
+			def: 180,
+			components: ["sergeant", "corporal", "corporal", "soldier", "soldier", "soldier", "soldier"],
+			drops: "towLauncher"
+		}
+	},
+	towSection: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#towSection-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#towSection-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#towSection-enemyDisplay'),
+			row: $('#towSection-IsEnemyRow')
+		},
+		stat: {
+			pow: 122,
+			def: 770,
+			components: ["staffSergeant", "towSquad", "towSquad", "towSquad", "towSquad"],
+			drops: "towLauncher"
+		}
+	},
+	antiArmourPlatoon: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#antiArmourPlatoon-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourPlatoon-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#antiArmourPlatoon-enemyDisplay'),
+			row: $('#antiArmourPlatoon-IsEnemyRow')
+		},
+		stat: {
+			pow: 198,
+			def: 1380,
+			components: ["antiArmourPlatoonHQ", "antiArmourSection", "towSection"],
+			drops: "towLauncher"
+		}
+	},
+	hmgSquad: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#hmgSquad-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgSquad-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgSquad-enemyDisplay'),
+			row: $('#hmgSquad-IsEnemyRow')
+		},
+		stat: {
+			pow: 16,
+			def: 110,
+			components: ["sergeant", "corporal", "soldier", "soldier"],
+			drops: "heavyMachineGun"
+		}
+	},
+	hmgSection: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#hmgSection-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgSection-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgSection-enemyDisplay'),
+			row: $('#hmgSection-IsEnemyRow')
+		},
+		stat: {
+			pow: 34,
+			def: 270,
+			components: ["staffSergeant", "hmgSquad", "hmgSquad"],
+			drops: "heavyMachineGun"
+		}
+	},
+	hmgPlatoonHQ: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#hmgPlatoonHQ-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgPlatoonHQ-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgPlatoonHQ-enemyDisplay'),
+			row: $('#hmgPlatoonHQ-IsEnemyRow')
+		},
+		stat: {
+			pow: 8,
+			def: 140,
+			components: ["lieutenant", "sergeant", "soldier", "soldier"],
+			drops: "rifle"
+		}
+	},
+	hmgPlatoon: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#hmgPlatoon-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgPlatoon-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#hmgPlatoon-enemyDisplay'),
+			row: $('#hmgPlatoon-IsEnemyRow')
+		},
+		stat: {
+			pow: 110,
+			def: 950,
+			components: ["hmgPlatoonHQ", "hmgSection", "hmgSection", "hmgSection"],
+			drops: "heavyMachineGun"
+		}
+	},
+	weaponsCompanyHQ: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#weaponsCompanyHQ-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#weaponsCompanyHQ-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#weaponsCompanyHQ-enemyDisplay'),
+			row: $('#weaponsCompanyHQ-IsEnemyRow')
+		},
+		stat: {
+			pow: 14,
+			def: 310,
+			components: ["major", "captain", "staffSergeant", "sergeant", "corporal", "soldier", "soldier"],
+			drops: "rifle"
+		}
+	},
+	weaponsCompany: {
+		home: {
+			total: 0,
+			current: 0,
+			currentDisplay: $('#weaponsCompany-SidePanelDisplay')
+		},
+		muster: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#weaponsCompany-musterDisplay')
+		},
+		enemy: {
+			total: 0,
+			current: 0,
+			temp: 0,
+			currentDisplay: $('#weaponsCompany-enemyDisplay'),
+			row: $('#weaponsCompany-IsEnemyRow')
+		},
+		stat: {
+			pow: 508,
+			def: 4090,
+			components: ["weaponsCompanyHQ", "heavyMortarPlatoon", "antiArmourPlatoon", "hmgPlatoon"],
+			drops: "rifle"
 		}
 	}
 };
